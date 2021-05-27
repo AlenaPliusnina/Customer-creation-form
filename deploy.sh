3d01df08325e8ab6decb4f7a82721f5b26edd74f
AlenaPliusnina <64252655+AlenaPliusnina@users.noreply.github.com>
2021-05-27 18:38:33 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:alenapliusnina/alenapliusnina.git master:gh-pages
+git push -f git@github.com:alenapliusnina/Customer-creation-form.git master:gh-pages
 
 cd -
