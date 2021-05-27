aa84c2d571e077bbc02a3b8cebf606ebf0e50cdd
AlenaPliusnina <64252655+AlenaPliusnina@users.noreply.github.com>
2021-05-27 18:35:47 +0300

    change public path

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    publicPath:
-	    process.env.NODE_ENV === 'production'
-    		? '/Customer-creation-form/'
-    		: '/',
+    publicPath: '/Customer-creation-form/',
     configureWebpack: {
 		resolve: {
 			alias: {
